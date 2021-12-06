d8c7f1de0b0b3dc81b725a7bbd98ddba0b56f2d6
dev2036 <devasnani2036@gmail.com>
2021-12-06 15:58:26 +0530

    thedevelopedweb

---
+++ js/main1.js
@@ -0,0 +1,11 @@
+var menu = document.getElementById("menu");
+function closeMenu(){
+    menu.style.top = "-100vh"
+}
+function openMenu(){
+    menu.style.top = "0vh"
+}
+//trial bases
+function closeMenu(){
+    menu.style.top = "-100vh"
+}
